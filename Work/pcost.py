047a59a15b0a33ee26bb38e8f1c1a5e49579e930
Androdev Rhel <andodev.rhel@gmail.com>
2026-04-29 21:08:50 -0400

    finished all exercises in 1.Introduction to Python (1.1 - 1.7)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,31 @@
 # pcost.py
 #
-# Exercise 1.27
-total_cost = 0
-with open('Data/portfolio.csv', 'rt') as f:
-    #cost of a stock = shares * price
-    next(f) # skip the header row
-    for line in f:
-        row = line.split(',')
-        total_cost += int(row[1]) * float(row[2])
+# Exercise 1.27 - 1.33
 
-print("Total cost", total_cost)
+import sys
+
+def portfolio_cost(filename):
+    total_cost = 0
+    line_counter = 0
+    with open(filename, 'rt') as f:
+        #cost of a stock = shares * price
+        next(f) # skip the header row
+        for line in f:
+            line_counter += 1
+            print(line_counter, line)
+            row = line.split(',')
+            try:
+                total_cost += int(row[1]) * float(row[2])
+            except ValueError as ve:
+                 print("The following Error occrued in line", line_counter, "of the file:", ve)
+            # finally:
+            #     print("in 'finally' block")
+            continue
+    return total_cost
+
+if len(sys.argv) == 2:
+    file_name = sys.argv[1]
+else:
+    file_name = 'Data/portfolio.csv'
+
+print("Total cost:", portfolio_cost(file_name))
